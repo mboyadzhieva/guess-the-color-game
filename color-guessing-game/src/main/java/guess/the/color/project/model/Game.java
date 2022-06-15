c9b7f20242653d0bb4d8ffc5f0015a02b8e3d347
mboyadzhieva <m.boyadzhieva99@gmail.com>
2022-06-15 10:09:06 +0300

    Getters and setters added, gitignore moved

--- color-guessing-game/src/main/java/guess/the/color/project/model/Game.java
+++ color-guessing-game/src/main/java/guess/the/color/project/model/Game.java
@@ -16,4 +16,84 @@ public class Game {
 	private int bestScore;
 	private int numOfStartingColors;
 	private boolean isWon;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public Complexity getComplexity() {
+		return complexity;
+	}
+	
+	public void setComplexity(Complexity complexity) {
+		this.complexity = complexity;
+	}
+	
+	public String getMainColor() {
+		return mainColor;
+	}
+	
+	public void setMainColor(String mainColor) {
+		this.mainColor = mainColor;
+	}
+	
+	public List<String> getColorsToChooseFrom() {
+		return colorsToChooseFrom;
+	}
+	
+	public void setColorsToChooseFrom(List<String> colorsToChooseFrom) {
+		this.colorsToChooseFrom = colorsToChooseFrom;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int getCurrentRound() {
+		return currentRound;
+	}
+	
+	public void setCurrentRound(int currentRound) {
+		this.currentRound = currentRound;
+	}
+	
+	public int getRounds() {
+		return rounds;
+	}
+	
+	public void setRounds(int rounds) {
+		this.rounds = rounds;
+	}
+	
+	public int getBestScore() {
+		return bestScore;
+	}
+	
+	public void setBestScore(int bestScore) {
+		this.bestScore = bestScore;
+	}
+	
+	public int getNumOfStartingColors() {
+		return numOfStartingColors;
+	}
+	
+	public void setNumOfStartingColors(int numOfStartingColors) {
+		this.numOfStartingColors = numOfStartingColors;
+	}
+	
+	public boolean isWon() {
+		return isWon;
+	}
+	
+	public void setWon(boolean isWon) {
+		this.isWon = isWon;
+	}
 }
